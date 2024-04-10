e2964fb8da455bf02cd0531ffe3cd833adff11ef
double2ci <122618934+leansevenuteen@users.noreply.github.com>
2024-04-10 23:32:06 +0700

    Update popup.js

--- text_capture/popup.js
+++ text_capture/popup.js
@@ -19,22 +19,10 @@ function getClipboardContent() {
             } else if (item.type === "text/plain") {
                 // Handle text content
                 item.getAsString(function(text) {
-                    let regex = /[0-9]+(\.[0-9]+)*/g;
-                    let phones = text.match(regex);
-                    let validPhones = [];
-                    if (phones) {
-                        for (let i = 0; i < phones.length; i++) {
-                            if (phones[i].length > 10) {
-                                validPhones.push(phones[i]);
-                            }
-                        }
-                    }
-                    if (validPhones.length > 0) {
-                        inputElement.value = validPhones.join(", ");
-                    }
-                    else {
-                        inputElement.value = "Dont have number phone."
-                    }
+                    let content = text.toLowerCase()
+                    let phone = capturePhones(content);
+                    let money = captureSalarys(content)
+                    inputElement.value = `${phone}\n${money}`;
                 });
             }            
         }
@@ -47,6 +35,41 @@ function getClipboardContent() {
     document.body.removeChild(tempInput);
 }
 
+function capturePhones(text) {
+    let regex = /[0-9]+(\.[0-9]+)*/g;
+    let phones = text.match(regex);
+    let validPhones = [];
+    if (phones) {
+        for (let i = 0; i < phones.length; i++) {
+            if (phones[i].length >= 10) {
+                validPhones.push(phones[i]);
+            }
+        }
+    }
+    if (validPhones.length > 0) {
+        return validPhones.join(", ");
+    }
+    else {
+        return "Dont have number phone.";
+    }
+}
+
+function captureSalarys(text) {
+    let regex = /\b(?:\d{1,3}(?:,\d{3})*(?:[.,]\d+)?|[\d.,]+)\s*(?:k|K|vnd|vnđ|tr|triệu)\b/g;
+    let salary = text.match(regex);
+    let validSalarys = [];
+    if (salary) {
+        for (let i = 0; i < salary.length; i++) {
+            validSalarys.push(salary[i]);
+        }
+    }
+    if (validSalarys.length > 0) {
+        return validSalarys.join(", ");
+    }
+    else {
+        return "Dont have salary.";
+    }
+}
 // Function to handle image blob
 function handleImageBlob(blob) {
     const imageUrl = URL.createObjectURL(blob);
