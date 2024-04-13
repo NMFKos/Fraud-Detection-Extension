6be26d1389f73c9bf88fb2a37bef26c72cbef568
Dương Đạt <144459605+NMFKos@users.noreply.github.com>
2024-04-13 22:01:15 +0700

    Add files via upload

---
+++ background.js
@@ -0,0 +1,19 @@
+function ensureSendMessage(tabId, message, callback){
+    chrome.tabs.sendMessage(tabId, {ping: true}, function(response){
+      if(response && response.pong) { // Content script ready
+        chrome.tabs.sendMessage(tabId, message, callback);
+      } else { // No listener on the other end
+        chrome.tabs.executeScript(tabId, {file: "content.js"}, function(){
+          if(chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+            throw Error("Unable to inject script into tab " + tabId);
+          }
+          // OK, now it's injected and ready
+          chrome.tabs.sendMessage(tabId, message, callback);
+        });
+      }
+    });
+  }
+chrome.action.onClicked.addListener((tab) =>{
+    chrome.scripting.executeScript(tab.id, {file: "content.js"});
+  });
