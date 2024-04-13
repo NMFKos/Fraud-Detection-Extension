9129fae16d47e057bb299e5dd3fe314ac6424ad6
Dương Đạt <144459605+NMFKos@users.noreply.github.com>
2024-04-13 10:02:34 +0700

    Add files via upload

--- YDCC/script.js
+++ YDCC/script.js
@@ -1,17 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
   var getSelectedTextButton = document.getElementById('getSelectedText');
   getSelectedTextButton.addEventListener('click', function() {
+    // Lấy tab hiện tại của người dùng
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {action: "getSelectedText"}, function(response) {
-        if (response && response.selectedText) {
-          alert(response.selectedText);
-        } else {
-          alert("No selected text found.");
+      // Lặp qua các tab trong cửa sổ hiện tại
+      for (let i = 0; i < tabs.length; i++) {
+        // Kiểm tra xem tab đó có đang hoạt động không
+        if (tabs[i].active) {
+          // Gửi tin nhắn đến tab hiện tại để lấy đoạn văn bản được chọn
+          chrome.tabs.sendMessage(tabs[i].id, {action: "getSelectedText"}, function(response) {
+            if (response && response.selectedText) {
+              alert(response.selectedText);
+            } else {
+              alert("No selected text found.");
+            }
+          });
+          break; // Dừng vòng lặp sau khi tìm thấy tab hiện tại
         }
-      });
+      }
     });
   });
 });
+
 
 // document.getElementById("myButton").addEventListener("click", highlightAndCheck);
 
